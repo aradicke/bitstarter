fa739e3e546260a88ce8f8714e1a5e3d14dcf53a
aradicke <aaron.radicke@gmail.com>
2013-07-12 04:18:48 +0000

    Index buffer attempt

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var hangar = new Buffer(256)
+hangar.readfileSync('index.html')
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!"');
+  response.send(hangar.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+}); 
